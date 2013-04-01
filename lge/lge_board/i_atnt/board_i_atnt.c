386ce7f5e551ceda2abe96745dc1abd43e74eb39
yiuflin <yiuflin@gmail.com>
2013-04-01 13:05:38 +0800

    Revert "i_atnt support kernel MSM thermal management"
    This reverts commit 64df01c8d5c87ec6852a890fb651586f287a9b50.

--- lge/lge_board/i_atnt/board_i_atnt.c
+++ lge/lge_board/i_atnt/board_i_atnt.c
@@ -82,7 +82,6 @@
 #include <mach/rpm-regulator.h>
 #include <mach/restart.h>
 #include <mach/board-msm8660.h>
-#include <linux/msm_tsens.h>
 
 #include "devices.h"
 #include "devices_i_atnt.h"
@@ -2415,13 +2414,6 @@ static struct platform_device *early_devices[] __initdata = {
 	&msm_device_dmov_adm1,
 };
 
-static struct tsens_platform_data pyr_tsens_pdata  = {
-                .tsens_factor           = 1000,
-                .hw_type                = MSM_8660,
-                .tsens_num_sensor       = 6,
-                .slope                  = 702,
-};
-
 static struct platform_device msm_tsens_device = {
 	.name   = "tsens-tm",
 	.id = -1,
@@ -2974,7 +2966,7 @@ static struct platform_device *surf_devices[] __initdata = {
 	&msm_device_rng,
 #endif
 
-	//&msm_tsens_device,
+	&msm_tsens_device,
 	&msm_rpm_device,
 #ifdef CONFIG_ION_MSM
 	&ion_dev,
@@ -6226,7 +6218,6 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 	uint32_t soc_platform_version;
 
 	pmic_reset_irq = PM8058_IRQ_BASE + PM8058_RESOUT_IRQ;
-	msm_tsens_early_init(&pyr_tsens_pdata);
 
 	/*
 	 * Initialize RPM first as other drivers and devices may need
