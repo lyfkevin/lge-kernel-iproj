0bd2d5d97b66f7e1981f7ae2df9ad882c16fcf4b
lyfkevin <yiuflin@gmail.com>
2013-05-07 20:07:53 +0800

    Add suspend_freq to userspace

--- arch/arm/mach-msm/cpufreq.c
+++ arch/arm/mach-msm/cpufreq.c
@@ -32,8 +32,9 @@
 
 #include "acpuclock.h"
 
-//the idea is to have this exported to userspace in the future
 #define SUSPEND_FREQ 702000
+static unsigned int suspend_freq = SUSPEND_FREQ;
+module_param(suspend_freq, int, 0644);
 
 #ifdef CONFIG_SMP
 struct cpufreq_work_struct {
@@ -131,8 +132,8 @@ static void cpulimit_early_suspend(struct early_suspend *handler)
 	int cpu;
 
 	for_each_possible_cpu(cpu) {
-		msm_cpufreq_set_freq_limits(cpu, MSM_CPUFREQ_NO_LIMIT, SUSPEND_FREQ);
-      		pr_info("Cpulimit: Early suspend - limit max frequency to: %d\n", SUSPEND_FREQ);
+		msm_cpufreq_set_freq_limits(cpu, MSM_CPUFREQ_NO_LIMIT, suspend_freq);
+      		pr_info("Cpulimit: Early suspend - limit max frequency to: %d\n", suspend_freq);
     	}
 	return;
 }
