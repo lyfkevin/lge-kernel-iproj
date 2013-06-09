66e62b72efe49d34cad3a73c77ff1932a24760be
lyfkevin <yiuflin@gmail.com>
2013-06-10 00:00:08 +0800

    board_i_skt.c:disable CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE

--- lge/lge_board/i_skt/board_i_skt.c
+++ lge/lge_board/i_skt/board_i_skt.c
@@ -118,11 +118,11 @@
 #ifdef CONFIG_LGE_WIRELESS_CHARGER_BQ24160
 #include <linux/bq24160-charger.h>
 #endif
-
+/*
 #ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
 	int set_two_phase_freq(int cpufreq);
 #endif
-
+*/
 #ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND_2_PHASE
 int id_set_two_phase_freq(int cpufreq);
 #endif
@@ -6325,11 +6325,11 @@ static void __init msm8x60_init(struct msm_board_data *board_data)
 			machine_is_msm8x60_fluid() ||
 			machine_is_msm8x60_dragon())
 		msm8x60_init_ebi2();
-
+/*
 #ifdef CONFIG_CPU_FREQ_GOV_ONDEMAND_2_PHASE
         set_two_phase_freq(1134000);
 #endif
-
+*/
 #ifdef CONFIG_CPU_FREQ_GOV_INTELLIDEMAND_2_PHASE
 	id_set_two_phase_freq(1134000);
 #endif
